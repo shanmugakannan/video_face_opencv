8b21a4e8958d74508d371cb3c08540c6fb827642
Shanmugakannan <shanmugakannan@gmail.com>
2016-06-10 18:27:19 +0530

    Update face.py

--- face.py
+++ face.py
@@ -1,8 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 33507))
+
 @app.route("/")
 def main():
     return "Upload video"
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=port)
